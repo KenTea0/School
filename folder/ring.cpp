8385347f5b67cd216fd758a4077551d121d7873f
KENTEA <131833594+KenTea0@users.noreply.github.com>
2024-06-05 09:09:58 +0900

    message:"committed!"

--- folder/ring.cpp
+++ folder/ring.cpp
@@ -2,23 +2,26 @@
 using namespace std;
 
 int n;
+//s1は調べたい文字列、s2は入力するためにある。
 string s1,s2;
 
 int main()
 {
     cout<<"ENTER THE WORDS PLS...>";
-	cin>>s1;
+	cin>>s1;    //input s1
     cout<<"ENTER THE NUMBER OF RING...>";
-    cin>>n;
+    cin>>n; //input rings
 
 
-	int ans=0;
+	int ans=0; //カウント用
 	for(int i=1;i<=n;i++)
 	{
-		cin>>s2;
-        s2+=s2;
+		cin>>s2; //10桁の文字列を入力
+        s2+=s2; 
 		if(s2.find(s1)!=s2.npos)++ans;
 	}
+    cout<<s2<<endl;
+    //結果の表示
 	cout<<"FOUND:"<<ans<<endl;
 	
 	return 0;
