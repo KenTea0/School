f9871bd191decc78ee48e9275551b3e50794523e
KENTEA <131833594+KenTea0@users.noreply.github.com>
2024-06-07 00:56:28 +0000

    finish!

--- sample2.cpp
+++ sample2.cpp
@@ -2,4 +2,18 @@
 #include <fstream>
 using namespace std;
 
-int main(){}
+int main(){
+    ifstream file("sample.txt");
+    if(file.is_open()){
+        int number;
+        string word;
+
+        while(file >> number >> word){
+            cout<<number<<"//"<<word<<endl;
+        }
+        file.close();
+    } else {
+        cerr<<"CAN NOT OPEN THE FILE..."<<endl;
+    }
+    return 0;
+}
